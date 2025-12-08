1262feb67f5076f111eb2474bb789ff8b73c5375
개강한 공대생 <162778323+verystrongdeveloper@users.noreply.github.com>
2025-12-08 14:56:52 +0900

    2×n 타일링 2

---
+++ c/11727.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int d[1001];
+    int n;
+    cin >> n;
+
+    d[1] = 1;
+    d[2] = 3;
+    for(int i = 3; i <= n; i++) {
+        d[i] = (d[i - 1] + 2 * d[i - 2]) % 10007;
+    }
+
+    cout << d[n];
+    return 0;
+}
